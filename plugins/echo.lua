be6ed3c67fa7ecabba49adb42b906ebe4cb733fa
parsaalemi <pa.alemi81@gmail.com>
2016-02-27 16:50:05 +0330

    Create echo.lua

---
+++ plugins/echo.lua
@@ -0,0 +1,36 @@
+[Forwarded from –=हईP.E.Y.M.A.Nईह=–]
+local function run(msg, matches)
+
+  local text = matches[1]
+
+  local b = 1
+
+
+  while b ~= 0 do
+
+    text = text:trim()
+
+    text,b = text:gsub('^!+','')
+
+  end
+
+  return text
+
+end
+
+
+return {
+
+  description = "Simplest plugin ever!",
+
+  usage = "!echo [whatever]: echoes the msg",
+
+  patterns = {
+
+    "^!echo +(.+)$"
+
+  }, 
+
+  run = run 
+
+}
